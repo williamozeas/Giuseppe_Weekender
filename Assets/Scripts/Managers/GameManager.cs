7c399471cdc5f3632df865da2eb258c3ed11cfd6
Will Ozeas <willatme@me.com>
2023-02-25 13:21:05 -0500

    Player movement feels pretty good!

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    Playing,
+    GameEnd,
+    BetweenLevels,
+    Pause,
+    Menu
+}
+
+public enum SceneNum
+{
+    MainMenu,
+    Level1,
+    Level2,
+    Level3
+}
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header("Data")]
+    private GameState _gamestate;
+    public GameState GameState //GameState cannot be set without calling SetGameState
+    {
+        set { SetGameState(value); }
+        get { return _gamestate; }
+    }
+    private SceneNum _currentScene;
+    public SceneNum CurrentScene //GameState cannot be set without calling SetGameState
+    {
+        set { LoadLevel(value); }
+        get { return _currentScene; }
+    }
+    
+    //References (Should set themselves in their Awake() functions)
+    private Player _player;
+    public Player Player => _player;
+    
+    //events - these can be recieved and trigger things all throughout the game
+    public static event Action GameStart;
+    public static event Action WaveStart;
+    public static event Action GameOver;
+    public static event Action GoToMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetGameState(GameState newGameState)
+    {
+        switch (newGameState)
+        {
+            case (GameState.Menu):
+            {
+                //TODO: reset variables
+                LoadLevel(SceneNum.MainMenu);
+                break;
+            }
+            case (GameState.Playing):
+            {
+                GameStart?.Invoke();
+                break;
+            }
+            case (GameState.GameEnd):
+            {
+                GameOver?.Invoke();
+                break;
+            }
+        }
+        _gamestate = newGameState;
+    }
+    
+    public void LoadLevel(SceneNum newScene)
+    {
+        switch (newScene)
+        {
+            case (SceneNum.MainMenu):
+            {
+                //load scene level
+                break;
+            }
+            case (SceneNum.Level1):
+            {
+                //load level 1
+                break;
+            }
+            case (SceneNum.Level2):
+            {
+                //load level 2
+                break;
+            }
+            case (SceneNum.Level3):
+            {
+                //load level 3
+                break;
+            }
+        }
+        _currentScene = newScene;
+    }
+
+    public void SetPlayer(Player newPlayer)
+    {
+        _player = newPlayer;
+    }
+}
---
+++ Assets/Scripts/Objects/Player.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+using UnityEngine.XR;
+
+
+public class Player : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    
+    private KCharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float gravityValue = -9.81f;
+    
+    //Awake is called before Start
+    private void Awake()
+    {
+        GameManager.Instance.SetPlayer(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<KCharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCharacterInput();
+    }
+    
+    private void HandleCharacterInput()
+    {
+        PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
+
+        // Build the CharacterInputs struct
+        characterInputs.MoveAxisRight = Input.GetAxisRaw("Horizontal");
+        characterInputs.JumpDown = Input.GetButtonDown("Jump");
+        // characterInputs.CrouchDown = Input.GetButtonDown("Crouch");
+        // characterInputs.CrouchUp = Input.GetButtonUp("Crouch");
+        
+        // PlayerCharacterInputs temp = new PlayerCharacterInputs();
+        // if (!temp.Equals(characterInputs))
+        // {
+        //     Debug.Log("oh!");
+        // }
+
+        // Apply inputs to character
+        controller.SetInputs(ref characterInputs);
+    }
+}
