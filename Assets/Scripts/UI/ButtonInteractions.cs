7c6edc4fcc86680e4dcf040b54520ea5c3a24258
joannetsaii <chihant@andrew.cmu.edu>
2023-03-20 12:33:55 -0400

    Set up button interaction script

---
+++ Assets/Scripts/UI/ButtonInteractions.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteractions : MonoBehaviour
+{
+    public GameObject MainBackground;
+    public GameObject LandingPage;
+    public GameObject HowToPlayPage;
+    public GameObject PlayPage;
+    // public GameObject EndingPage;
+
+    public void clickStartFromLanding() {
+        LandingPage.gameObject.SetActive(false);
+        HowToPlayPage.gameObject.SetActive(false);
+        PlayPage.gameObject.SetActive(true);
+        // EndingPage.gameObject.SetActive(false);
+        MainBackground.SetActive(false);
+    }
+
+    public void clickHowToPlayFromMain() {
+        LandingPage.gameObject.SetActive(false);
+        HowToPlayPage.gameObject.SetActive(true);
+        PlayPage.gameObject.SetActive(false);
+        // EndingPage.gameObject.SetActive(false);
+    }
+
+    public void clickStartFromHowToPlay() {
+        LandingPage.gameObject.SetActive(false);
+        HowToPlayPage.gameObject.SetActive(false);
+        PlayPage.gameObject.SetActive(true);
+        // EndingPage.gameObject.SetActive(false);
+        MainBackground.SetActive(false);
+    }
+
+    public void clickBackFromHowToPlay() {
+        LandingPage.gameObject.SetActive(true);
+        HowToPlayPage.gameObject.SetActive(false);
+        PlayPage.gameObject.SetActive(false);
+        // EndingPage.gameObject.SetActive(false);
+        MainBackground.SetActive(true);
+    }
+
+    public void clickExitFromEnd() {
+        // GameManager.Instance.GameState = GameState.Menu;
+        
+        LandingPage.gameObject.SetActive(true);
+        HowToPlayPage.gameObject.SetActive(false);
+        PlayPage.gameObject.SetActive(false);
+        // EndingPage.gameObject.SetActive(false);
+        MainBackground.SetActive(true);
+    }
+
+    public void clickPlayAgainFromEnd() {
+        // GameManager.Instance.GameState = GameState.Menu;
+        // GameManager.Instance.GameState = GameState.Playing;
+        
+        LandingPage.gameObject.SetActive(false);
+        HowToPlayPage.gameObject.SetActive(false);
+        PlayPage.gameObject.SetActive(true);
+        // EndingPage.gameObject.SetActive(false);
+        MainBackground.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainBackground.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
