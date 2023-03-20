9ac36257dd86fd7dead4d2cc521bfc8f377d57ea
Will Ozeas <willatme@me.com>
2023-03-20 18:13:33 -0400

    Sound effect rewinder fully works

--- Assets/Scripts/Audio/SoundEffectTracker.cs
+++ Assets/Scripts/Audio/SoundEffectTracker.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SoundEffectTracker : MonoBehaviour
 {
     public Timeline Timeline = Timeline.World;
-    private Stack<ReversibleSoundEffect> _sfx = new Stack<ReversibleSoundEffect>();
+    private List<ReversibleSoundEffect> _sfx = new List<ReversibleSoundEffect>();
     private List<ReversibleSoundEffect> _playingSFX = new List<ReversibleSoundEffect>();
-    private List<ReversibleSoundEffect> _reversedSfxToAdd = new List<ReversibleSoundEffect>();
 
     private bool _justRewinded = false;
 
@@ -33,27 +33,36 @@ public class SoundEffectTracker : MonoBehaviour
     {
         //replaying sounds
         //TODO: currently this doesn't work with player time sounds because there is no "player timer"
-        ReversibleSoundEffect peek;
+        // ReversibleSoundEffect peek;
         if (((Timeline == Timeline.World && RewindManager.IsBeingRewinded) //world rewound
              || (Timeline == Timeline.Player && GameManager.Instance.Player.PlayerRewinder.IsBeingRewinded)) //or player rewound
-            && _sfx.TryPeek(out peek) && GameManager.Instance.Time < peek.times.Item2) //and we're at the correct time
+            && _sfx.Count > _playingSFX.Count) //and we're at the correct time
         {
-            //TODO: right now if you start to reverse, then go forwards, then reverse again it will not play
-            if (!_justRewinded)
+            ReversibleSoundEffect peek = _sfx[_sfx.Count - 1 - _playingSFX.Count];
+            if (GameManager.Instance.Time < peek.times.Item2)
             {
-                _sfx.Pop();
-                peek.OnReverse();
-                _reversedSfxToAdd.Add(peek);
+                //TODO: right now if you start to reverse, then go forwards, then reverse again it will not play
+                if (!_justRewinded)
+                {
+                    peek.OnReverse();
+                }
+                _playingSFX.Add(peek);
             }
-            _playingSFX.Add(peek);
         }
 
         for (int i = _playingSFX.Count - 1; i >= 0; i--)
         {
             ReversibleSoundEffect sfx = _playingSFX[i];
-            if (sfx.times.Item1 > GameManager.Instance.Time || sfx.times.Item2 < GameManager.Instance.Time)
+            if (sfx.times.Item2 < GameManager.Instance.Time)
             {
                 _playingSFX.RemoveAt(i);
+                continue;
+            }
+
+            if (sfx.times.Item1 > GameManager.Instance.Time)
+            {
+                _playingSFX.RemoveAt(i);
+                _sfx.Remove(sfx);
                 continue;
             }
 
@@ -93,7 +102,7 @@ public class SoundEffectTracker : MonoBehaviour
 
     public void AddSfx(ReversibleSoundEffect newSfx)
     {
-        _sfx.Push(newSfx);
+        _sfx.Add(newSfx);
     }
 
     public void AddToPlaying(ReversibleSoundEffect newSfx)
