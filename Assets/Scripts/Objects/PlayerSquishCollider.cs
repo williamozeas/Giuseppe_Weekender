d9f686cc159dfe262fa4423e850a2e5201d543bd
EfeCekirge <efecekirge@gmail.com>
2023-03-21 23:53:06 -0400

    Buttons are working but no time travel on them

--- Assets/Scripts/Objects/PlayerSquishCollider.cs
+++ Assets/Scripts/Objects/PlayerSquishCollider.cs
@@ -7,6 +7,7 @@ public class PlayerSquishCollider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collider)
     {
+        Debug.Log(collider.name);
         //because squishing is caused by objects running into the player, the player should rewind world time
         //hence we use the same code as running out of time
         if(!RewindManager.IsBeingRewinded && !collider.TryGetComponent<DeathCollider>(out var coll))
