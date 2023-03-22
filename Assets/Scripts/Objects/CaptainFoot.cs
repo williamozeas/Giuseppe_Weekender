e611630c61350a1b0f130986aa4d32a6d19a126e
Will Ozeas <willatme@me.com>
2023-03-22 16:24:02 -0400

    Captain's foot is fully rewindable

--- Assets/Scripts/Objects/CaptainFoot.cs
+++ Assets/Scripts/Objects/CaptainFoot.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class CaptainFoot : MonoBehaviour
+public class CaptainFoot : RewindAbstract
 {
-    enum StompPhase
+    public enum StompPhase
     {
         Tracking,
         Windup,
@@ -14,6 +14,13 @@ public class CaptainFoot : MonoBehaviour
         Rest,
         Recovery
     }
+    
+    public struct CaptainState
+    {
+        public StompPhase phase;
+        public float timer;
+    }
+    CircularBuffer<CaptainState> trackedState; 
 
     private StompPhase phase = StompPhase.Tracking;
     
@@ -26,6 +33,8 @@ public class CaptainFoot : MonoBehaviour
     public List<Transform> raycastOrigins = new List<Transform>();
     
     private float timer = 0;
+    public float Timer => timer;
+    
     private float xTarget;
     private float velocity;
     private float acceleration;
@@ -41,76 +50,89 @@ public class CaptainFoot : MonoBehaviour
     {
         player = GameManager.Instance.Player;
         rb = GetComponent<Rigidbody>();
+        trackedState = new CircularBuffer<CaptainState>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    protected override void FixedUpdate()
     {
-        switch (phase)
+        if (!RewindManager.IsBeingRewinded)
         {
-            case StompPhase.Tracking:
+            switch (phase)
             {
-                xTarget = player.transform.position.x;
+                case StompPhase.Tracking:
+                {
+                    xTarget = player.transform.position.x;
 
-                acceleration = Mathf.Pow(timer / trackingTime, 2);
+                    acceleration = Mathf.Pow(timer / trackingTime, 2);
 
-                velocity = (xTarget - transform.position.x) * acceleration * 20f;
-                float newPosX = transform.position.x + velocity * Time.fixedDeltaTime;
-                rb.MovePosition(new Vector3(newPosX, transform.position.y, transform.position.z));
-                timer += Time.fixedDeltaTime;
-                if (timer > trackingTime)
-                {
-                    StartWindup();
-                }
-                break;
-            }
-            case StompPhase.Windup:
-            {
-                timer += Time.fixedDeltaTime;
-                float newPosY = transform.position.y + Time.fixedDeltaTime;
-                rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
-                if (timer > windupTime)
-                {
-                    StartStomp();
-                }
-                break;
-            }
-            case StompPhase.Stomp:
-            {
-                timer += Time.fixedDeltaTime;
-                if (timer > stompTime)
-                {
-                    rb.MovePosition(new Vector3(transform.position.x, originalHeight - distanceToStomp, transform.position.z));
-                    StartRest();
+                    velocity = (xTarget - transform.position.x) * acceleration * 20f;
+                    float newPosX = transform.position.x + velocity * Time.fixedDeltaTime;
+                    rb.MovePosition(new Vector3(newPosX, transform.position.y, transform.position.z));
+                    timer += Time.fixedDeltaTime;
+                    if (timer > trackingTime)
+                    {
+                        StartWindup();
+                    }
+
                     break;
                 }
+                case StompPhase.Windup:
+                {
+                    timer += Time.fixedDeltaTime;
+                    float newPosY = transform.position.y + Time.fixedDeltaTime;
+                    rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
+                    if (timer > windupTime)
+                    {
+                        StartStomp();
+                    }
 
-                float newPosY = EasingFunction.EaseInQuad(stompStartHeight, originalHeight - distanceToStomp, timer/stompTime);
-                Debug.Log(newPosY);
-                rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
-                break;
-            }
-            case StompPhase.Rest:
-            {
-                timer += Time.fixedDeltaTime;
-                if (timer > restTime)
-                {
-                    StartRecover();
+                    break;
                 }
-                break;
-            }
-            case StompPhase.Recovery:
-            {
-                timer += Time.fixedDeltaTime;
-                float newPosY = EasingFunction.EaseInOutCubic(recoveryStartPos, originalHeight, timer/recoveryTime);
-                rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
-                if (timer > recoveryTime)
+                case StompPhase.Stomp:
                 {
-                    StartTrack();
+                    timer += Time.fixedDeltaTime;
+                    if (timer > stompTime)
+                    {
+                        rb.MovePosition(new Vector3(transform.position.x, originalHeight - distanceToStomp,
+                            transform.position.z));
+                        StartRest();
+                        break;
+                    }
+
+                    float newPosY = EasingFunction.EaseInQuad(stompStartHeight, originalHeight - distanceToStomp,
+                        timer / stompTime);
+                    Debug.Log(newPosY);
+                    rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
+                    break;
                 }
-                break;
+                case StompPhase.Rest:
+                {
+                    timer += Time.fixedDeltaTime;
+                    if (timer > restTime)
+                    {
+                        StartRecover();
+                    }
+
+                    break;
+                }
+                case StompPhase.Recovery:
+                {
+                    timer += Time.fixedDeltaTime;
+                    float newPosY =
+                        EasingFunction.EaseInOutCubic(recoveryStartPos, originalHeight, timer / recoveryTime);
+                    rb.MovePosition(new Vector3(transform.position.x, newPosY, transform.position.z));
+                    if (timer > recoveryTime)
+                    {
+                        StartTrack();
+                    }
+
+                    break;
+                }
             }
         }
+        
+        base.FixedUpdate();
     }
 
     void StartWindup()
@@ -156,5 +178,36 @@ public class CaptainFoot : MonoBehaviour
     {
         timer = 0;
         phase = StompPhase.Tracking;
+    }
+
+    //In this method define what will be tracked. In our case we want to track already implemented audio tracking,particle tracking + new custom timer tracking
+    protected override void Track()
+    {
+        TrackCaptainState();
+    }
+
+    //In this method define, what will be restored on time rewinding. In our case we want to restore Particles, Audio and custom implemented Timer
+    protected override void Rewind(float seconds)
+    {
+        RestoreCaptainState(seconds);
+    }
+
+
+    // This is an example of custom variable tracking
+    public void TrackCaptainState()
+    {
+        CaptainState state = new CaptainState();
+        state.phase = phase;
+        state.timer = timer;
+        trackedState.WriteLastValue(state);
+    }
+
+
+    // This is an example of custom variable restoring
+    public void RestoreCaptainState(float seconds)
+    {
+        CaptainState rewindValue = trackedState.ReadFromBuffer(seconds);
+        timer = rewindValue.timer;
+        phase = rewindValue.phase;
     }
 }
