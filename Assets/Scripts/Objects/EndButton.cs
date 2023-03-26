f78b9060d430f8656f468220f4976e0b44aa6a4a
EfeCekirge <efecekirge@gmail.com>
2023-03-26 19:42:25 -0400

    Main Menu Button

---
+++ Assets/Scripts/Objects/EndButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndButton : MonoBehaviour
+{
+
+    [SerializeField] private SceneNum scene;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Foot"))
+        {
+            GameManager.Instance.CurrentScene = scene;
+        }
+    }
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Foot"))
+        {
+            GameManager.Instance.CurrentScene = scene;
+        }
+    }
+}
